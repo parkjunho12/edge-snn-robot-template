4d1e9298a7b452fceffddfa2bdfce60bbcfab565
Junho Park <ghdlwnsgh12@naver.com>
2025-11-12 13:58:40 +0000

    [12/11/2025] - add github action yml file

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# ROS 환경 로드
+source /opt/ros/humble/setup.bash
+
+# 워킹 디렉토리로 이동
+cd /lab
+
+# uvicorn 실행 (FastAPI 앱 경로 맞추기)
+exec uvicorn src.infer_server.app:app --host 0.0.0.0 --port 8000
