5cafc9cdfd88cc0807e8cf1e58c83e02a0fc2b80
Junho Park <ghdlwnsgh12@naver.com>
2025-11-18 15:41:11 +0000

    [18/11/25] #patch modify code

--- src/semg/ninapro.py
+++ src/semg/ninapro.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional
 
 import numpy as np
 from numpy.typing import NDArray
